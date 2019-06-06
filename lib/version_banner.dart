66b25cc769f7cb2aa5ea33b1c4f49c63ef593369
vanethos <solid.goncalo@gmail.com>
2019-06-06 15:42:36 +0100

    Initial Commit

---
+++ lib/version_banner.dart
@@ -0,0 +1,34 @@
+library version_banner;
+
+import 'package:flutter/widgets.dart';
+
+class VersionBanner extends StatelessWidget {
+  Widget child;
+  BannerLocation location;
+  TextStyle textStyle;
+  String text;
+  Color color;
+
+  VersionBanner(
+      {@required this.child,
+      @required this.text,
+      this.location = BannerLocation.topEnd,
+      this.textStyle = const TextStyle(
+          color: Color.fromARGB(255, 255, 255, 255),
+          fontWeight: FontWeight.bold),
+      this.color = const Color.fromARGB(255, 255, 0, 0)});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Banner(
+        color: color,
+        message: text,
+        location: location,
+        textStyle: textStyle,
+        child: child,
+      ),
+    );
+  }
+}
---
+++ test/version_banner_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:version_banner/version_banner.dart';
+
+void main() {
+}
