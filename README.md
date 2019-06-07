42868a67514b0d9bf5bf606d72884bc0ae713716
vanethos <solid.goncalo@gmail.com>
2019-06-07 18:24:36 +0100

    Change readme Add check with package name to hide or show banner Add visibility parameter

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ This widget should wrap `MaterialApp` or `CupertinoApp`.
 ```dart
 VersionBanner(
           text: "Yay!",
+          packageExtensions: [".dev"],
           child: MaterialApp(
                        debugShowCheckedModeBanner: false,
                        title: 'Flutter Demo',
@@ -24,46 +25,13 @@ VersionBanner(
       );
 ```
 
+`packageExtensions` will check the app's package name with the given array. If the package name contains part of the String in the array, then the banner is visible.
+
 The following properties can be changed:
 
-* `color`
-* `textStyle`
-* `text`
-* `location`
-
-This package can also be used with [package_info](https://pub.dev/packages/package_info) to know what is the current flavour of the app and show/hide the banner accordingly
-
-```dart
-void main() async {
-  var packageInfo = await PackageInfo.fromPlatform();
-  var packageName = packageInfo.packageName;
-  runApp(MyApp(packageName));
-}
-
-class MyApp extends StatelessWidget {
-  String packageName;
-  
-  MyApp(this.packageName);
-
-  @override
-  Widget build(BuildContext context) {
-    var materialApp = MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-
-    if (packageName.contains("dev") || packageName.contains("alpha")) {
-      return VersionBanner(
-          text: "Yay!",
-          child: materialApp
-      );
-    }
-
-    return materialApp;
-  }
-}
-```
+* `color` - Banner's color
+* `textStyle` -  Banner's text style
+* `text` - The text to appear. If set to null will show the `pubspec` version
+* `location` - the banner's location on the screen, based on the class [BannerLocation](https://api.flutter.dev/flutter/widgets/BannerLocation-class.html)
+* `packageExtensions` - will check the app's package name with the given array. If the package name contains part of the String in the array, then the banner is visible.
+* `visible` - decides if the banner is visible or not. If set to `false`, it overrides the `packageExtensions` attribute
--- lib/version_banner.dart
+++ lib/version_banner.dart
@@ -1,6 +1,7 @@
 library version_banner;
 
 import 'package:flutter/widgets.dart';
+import 'package:package_info/package_info.dart';
 
 /// Version Banner Widget
 ///
@@ -68,26 +69,87 @@ class VersionBanner extends StatelessWidget {
   /// Has a default value of Red
   Color color;
 
+  /// Visibility of the banner
+  bool visible;
+
+  /// List of package extensions to hide or show
+  ///
+  /// Checks if the package name has a string in the array, eg.:
+  ///   `com.vanethos.example.dev` with array `["dev"]`
+  ///
+  /// This property is overriden by [visible]. If [visible] is false,
+  /// the banner will not be shown.
+  List<String> packageExtensions;
+
   VersionBanner(
       {@required this.child,
-      @required this.text,
+      this.text,
       this.location = BannerLocation.topEnd,
       this.textStyle = const TextStyle(
           color: Color.fromARGB(255, 255, 255, 255),
           fontWeight: FontWeight.bold),
-      this.color = const Color.fromARGB(255, 255, 0, 0)});
+      this.color = const Color.fromARGB(255, 255, 0, 0),
+      this.visible = true, this.packageExtensions});
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Banner(
-        color: color,
-        message: text,
-        location: location,
-        textStyle: textStyle,
-        child: child,
-      ),
-    );
+    if (visible) {
+      /// If the banner is visible, we need to check if we are going to present
+      /// the default text OR if we are going to check the visibility of the
+      /// banner via the package extensions.
+      /// If this is the case, we will need to access the package_info plugin
+      if (text == null || packageExtensions!= null) {
+        return FutureBuilder<PackageInfo>(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var isDev = false;
+                if (packageExtensions != null) {
+                  for (var package in packageExtensions) {
+                    if (snapshot.data.packageName.contains(package)) {
+                      isDev = true;
+                      break;
+                    }
+                  }
+                } else {
+                  isDev = true;
+                }
+
+                if (isDev) {
+                  return Directionality(
+                    textDirection: TextDirection.ltr,
+                    child: Banner(
+                      color: color,
+                      message: text ?? snapshot.data.version,
+                      location: location,
+                      textStyle: textStyle,
+                      child: child,
+                    ),
+                  );
+                }
+              }
+
+              /// If we are not in a development package, return the original child
+              return child;
+            }
+        );
+      }
+
+      /// If we don't use the default text or search the package names, then
+      /// we return the banner with the user's input
+      return Directionality(
+        textDirection: TextDirection.ltr,
+        child: Banner(
+          color: color,
+          message: text,
+          location: location,
+          textStyle: textStyle,
+          child: child,
+        ),
+      );
+    }
+
+    /// The banner is not visible, return the child instead
+    return child;
   }
 }
